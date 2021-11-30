b969bc76339e3dc20412a00f35b9f2498a1f791a
kzhctech <95127984+kzhctech@users.noreply.github.com>
2021-11-30 08:18:04 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const cheerio = require('cheerio')
+const axios = require('axios')
+
+axios.get('https://m.cricbuzz.com/cricket-commentary/39806/sl-vs-wi-2nd-test-west-indies-tour-of-sri-lanka-2021').then((response) => {
+  // Load the web page source code into a cheerio instance
+  const $ = cheerio.load(response.data)
+  const urlElems = $('#paginationList').children().text()
+  console.log(urlElems)
+})
+
+
+//#paginationList > div > div:nth-child(4) > div > div > div > p
