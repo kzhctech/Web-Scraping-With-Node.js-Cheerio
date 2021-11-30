58f11d5dfcc6c0b982e5f8c93613afae8a9247be
kzhctech <95127984+kzhctech@users.noreply.github.com>
2021-11-30 08:38:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 axios.get('https://m.cricbuzz.com/cricket-commentary/39806/sl-vs-wi-2nd-test-west-indies-tour-of-sri-lanka-2021').then((response) => {
   // Load the web page source code into a cheerio instance
   const $ = cheerio.load(response.data)
-  const urlElems = $('#paginationList').children().text()
+  const urlElems = $('#paginationList').first().first().first().first().children().first().first().children().children().children().children().children().children().children().first().text()
   console.log(urlElems)
 })
 
