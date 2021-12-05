7102778909b909aecf5fd2702c406b81d58e32bc
kzhctech <95127984+kzhctech@users.noreply.github.com>
2021-12-05 05:02:26 +0000

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,41 +1,21 @@
-// Babak Habibi - babak.habibi20@gmail.com
-// Including necessary libraries
-const request = require('request');
-const cheerio = require('cheerio');
 
-// File system is inside the NodeJS
-const fs = require('fs');
-// Write our data in CSV file
-const writeStream = fs.createWriteStream('post.csv');
+const cheerio = require('cheerio')
+const axios = require('axios')
 
-
-// Write header for excel file
-writeStream.write('Title,Description \n');
-
-// Requesting the URL with 3 default parameters
-request('https://www.huffingtonpost.com/topic/italy-travel', (error,
-                                                              response,
-                                                              html) => {
-    if (!error && response.statusCode === 200) {
-        // Use cheerio to parse and create the jQuery-like DOM
-        const $ = cheerio.load(html);
-
-        // Finding targeted elements
-        $('.card__content').each((i, el) => {
-            const title = $(el)
-                .find('.card__headline__text')
-                .text()
-                .replace(/["',]/g, "")
-                .trim();
-
-            const desc = $(el).find('.card__image__src').attr('src').split('?')[0];
+axios.get('https://m.cricbuzz.com/cricket-commentary/38587/ind-vs-nz-2nd-test-new-zealand-tour-of-india-2021').then((response) => {
+  // Load the web page source code into a cheerio instance
+  const $ = cheerio.load(response.data)
+  //const urlElems = $('.list-content span:nth-child(5)').text()
+  //const urlElems = $('.cbz-ui-status').text()
+ // const urlElems = $('#top table tr:nth-child(3)').first().first().find('td:nth-child(2)').text()
+// const urlElems = $('#top .ui-match-scores-branding h3').text()
+ // const urlElems = $('#top .ui-match-scores-branding .crr').text()
+  //const urlElems = $('#paginationList').first().first().first().first().children().first().first().children().children().children().children().children().first().text()
+  console.log(urlElems)
+})
 
 
-            // Write Row to CSV
-            writeStream.write(`${title}, ${desc} \n`);
-
-        });
-        // Making sure everything is alright
-        console.log('its done');
-    }
-});
+//#paginationList > div > div:nth-child(4) > div > div > div > p
+//#top > div > div:nth-child(11) > div:nth-child(2) > div > div > div > div > table > tbody > tr:nth-child(2) > td:nth-child(5)
+//#top > div > div:nth-child(10) > div > div > h3 > div
+//#top > div > div:nth-child(11) > div.cb-list-item.miniscore-data.ui-branding-style.ui-branding-style-partner > div > div > div > span:nth-child(5)
